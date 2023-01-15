25b5943907cafb3b3622b8725625a2d62840b84a
Beko K <101501854+amaym0nn@users.noreply.github.com>
2023-01-15 20:22:30 +0300

    Update fuzz.c

--- fuzz.c
+++ fuzz.c
@@ -29,12 +29,13 @@ int main() {
                           }
                             int i = 0;
                              for(; i < nm + 8; i++) {
-                                // Bu döngüde nm+8 (108) kadar buff değişkenine "A" değerini ekler. Fakat bu döngüden çıkıp tekrar geldiğinde  bu arttırma işlemi iki katına çıkar.
-                                // Mesela bu döngüde 100 kere arttırıp döngüden çıktıktan sonra nm'yi 100 kere arttırdıktan sonra tekrar bu döngüye geldiğinde 200 olacaktır. Böyle böyle artacaktır.
+                                /* Bu döngüde nm+8 (108) kadar buff değişkenine "A" değerini ekler. Fakat bu döngüden çıkıp tekrar geldiğinde  bu arttırma işlemi iki katına çıkar.
+                                Mesela bu döngüde 100 kere arttırıp döngüden çıktıktan sonra nm'yi 100 kere arttırdıktan sonra tekrar bu döngüye geldiğinde 200 olacaktır. Böyle böyle artacaktır.
+                                */
                                 strcat(buff, "A");  // Burada buff değişkenine nm+8 kere A değerini ekler. Mesela sonuç şu şekilde olur: TRUN /.:/AAAAAAAAAAAAA gibi. 
                                 printf("Buff Lenght: %d\n", strlen(buff));  // Burada program hangi uzunlukta crash verdiğini görebilmek için buff değişkenin sürekli aktif olarak uzunluğu ekrana bastırılıyor
                              }
-                             int wr = write(scket, buff, sizeof(buff)); // burada ise write komutu kullanılarak oluşturulan sockete buff değişkeninin gönderir. 
+                             int wr = write(scket, buff, sizeof(buff)); // burada ise write komutu kullanılarak oluşturulan sockete buff değişkeni gönderir. 
                                 if (wr == -1) {
                                     printf("Crashed At");
                                     close(scket);
